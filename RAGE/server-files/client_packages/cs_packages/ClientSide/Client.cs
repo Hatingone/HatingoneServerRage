cc188ff6f6137d27e8f212629575560eec759a27
Hatingone <egorkukushkin51@gmail.com>
2023-06-05 16:50:57 +0300

    AddShareData
    I add level for player

--- RAGE/server-files/client_packages/cs_packages/ClientSide/Client.cs
+++ RAGE/server-files/client_packages/cs_packages/ClientSide/Client.cs
@@ -1,13 +1,27 @@
 ﻿using RAGE;
 using System;
+using RAGE.Elements;
+using RAGE.Ui;
 
 namespace ClientSide
 {
     public class Client : Events.Script
     {
+
+        private const string _levelKey = nameof(_levelKey);// создаём такой же ключ как и в Level для того чтобы передать его в методе LevelAllPlayers как значение уровня у игрока 
+
         public Client()
         {
             RAGE.Chat.Output("Hello World! Client");
+            RAGE.Input.Bind(VirtualKeys.N, true, LevelAllPlayers);//при нажатии кнопки N у нас вызывается метод LevelAllPlayers
+        }
+
+        private void LevelAllPlayers ()
+        {
+            foreach (var player in RAGE.Elements.Entities.Players.All)//здесь мы обращаемся к классу Entities чтобы потом обратиться к Players, а дальше с помощью All преобразовать всё это в list. Чтобы нам выдало список игроков с уровнями.
+            {
+                Chat.Output(player.Name + "------" + player._GetSharedData<int>(_levelKey) + "lvl");//выводим в чат список игроков: их имена и лвл
+            }
         }
     }
 }
---
+++ RAGE/server-files/dotnet/resources/ServerSide/Level.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace ServerSide
+{
+    internal class Level : Script
+    {
+
+        private static string _levelKey = nameof(_levelKey);
+
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SetSharedData(_levelKey, new Random().Next(1, 100));//когда игрок присоединяется ему выдаётся рандомный уровень от 1 до 100 
+        }
+    }
+}
+//здесь у нас будет прописан лвл игрока. Здесь будет почти такой же принцип что и с PrivateVehicle(ключ и т.д.)
