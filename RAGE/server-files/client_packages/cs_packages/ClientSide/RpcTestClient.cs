0c2fd076996bf78677b007e83fe543e829655d97
Hatingone <egorkukushkin51@gmail.com>
2023-06-08 18:30:23 +0300

    TestRpc
    Test Rpc

---
+++ RAGE/server-files/client_packages/cs_packages/ClientSide/RpcTestClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RAGE;
+using RAGE.Elements;
+using RAGE.Ui;
+
+namespace ClientSide
+{
+    public class RpcTestClient : Events.Script
+    {
+
+        private const string _serverTime = nameof(_serverTime);
+
+         public RpcTestClient() 
+        {
+            Input.Bind(VirtualKeys.F2, true, GetTimeServer);
+        } 
+
+        private async void GetTimeServer()
+        {
+
+        }
+    }
+}
---
+++ RAGE/server-files/dotnet/resources/ServerSide/RpcTestServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GTANetworkAPI;
+using RAGE;
+
+namespace ServerSide
+{
+    public class RpcTestServer : Script
+    {
+        private object GetTime(Player player)
+        {
+            return DateTime.Now;
+        }
+    }
+}
