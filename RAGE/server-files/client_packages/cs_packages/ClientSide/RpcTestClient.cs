2c25ccec6479fce0ca80e3745968a291c8523594
Hatingone <egorkukushkin51@gmail.com>
2023-06-10 20:43:56 +0300

    FpsPlayer
    When you type in chat "/GetFPSPlayer 0" you can see fps of any player(depending on which player id you type)

--- RAGE/server-files/client_packages/cs_packages/ClientSide/RpcTestClient.cs
+++ RAGE/server-files/client_packages/cs_packages/ClientSide/RpcTestClient.cs
@@ -9,10 +9,17 @@ namespace ClientSide
     {
 
         private const string GetTimeServerKey = "RPC::CLIENT:SERVER:GetTimeServer";//ключ для вызова процедуры. Здесь принцип очень схож с ивентами. У нас здесь тоже есть ключ но он для вызова процедуры. Здесь порядок наименования схож с тем что я сделал в файле Events, в методе RepairCar: ЛюбоеНазавание::КтоВызываетПроцедуру::КтоЕёОбрабатывает::ЧтоНужноСделатьИлиНазваниеПроцедуры. У меня это выглядит так "RPC::CLIENT:SERVER:GetTimeServer"
+        private const string GetFpsPlayerKey = "RPC::SERVER:CLIENT:GetFPSClient";
 
         public RpcTestClient() 
         {
             Input.Bind(VirtualKeys.F2, true, GetTimeServer);//бинд для вызова просмотра серверного времени
+            Events.AddProc(GetFpsPlayerKey, args  => //сначала передаём ключ, дальше нам приходят аргументы в args которые мы уже обрабатываем
+            {
+
+                return 1 / RAGE.Game.Misc.GetFrameTime();//возвращаем с помощью RAGE.Game.Misc получаем фпс игрока(текущий). И нам возращает int значение. В самом начале делим на 1 чтобы это всё работало
+
+            });//регестрируем процедуру. В которой нам просто приходит значение фпс игрока от клиента.
         } 
 
         private async void GetTimeServer()//метод для получения серверного времени. Делаем его ассинхронным для получения ответа от сервера.
@@ -24,5 +31,6 @@ namespace ClientSide
             //выводим в чат игрока время
             Chat.Output(text: $"Time server = {date.ToShortTimeString()}");
         }
+
     }
 }
--- RAGE/server-files/dotnet/resources/ServerSide/RpcTestServer.cs
+++ RAGE/server-files/dotnet/resources/ServerSide/RpcTestServer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using GTANetworkAPI;
 
 namespace ServerSide
@@ -7,12 +8,31 @@ namespace ServerSide
     {
 
         private const string GetTimeServerKey = "RPC::CLIENT:SERVER:GetTimeServer";//делаем такой же ключ как и в RpcTestClient для работы с процедурой 
-
+        private const string GetFpsPlayerKey = "RPC::SERVER:CLIENT:GetFPSClient";//клбч для работы с фпс игрока
 
         [RemoteProc(GetTimeServerKey)]//помечаем метод ниже как и с ивентами только теперь пишем RemoteProc
         private string GetTime(Player player)//тест метод для передачи серверного времени на клиент
         {
             return NAPI.Util.ToJson(DateTime.Now);
+        }
+
+        [Command]
+        private void GetFPSPlayer(Player player, int id)
+        {
+            var playerFps = NAPI.Pools.GetAllPlayers().FirstOrDefault(x => x.Id == id);//нам нужно получить игрока через пул GetAllPlayers. И затем с помощью FirstOrDefault в котором будем искать объект по айдишнику
+            if (playerFps == null)//проверяем есть ли игрок 
+            {
+                player.SendChatMessage("Player not found");
+                return;
+            }
+            //так как любой вызов процедуры должен быть ассинхронный, то запускаем новый task чтобы не делать метод ассинхроным(запускаем его здесь). И для этого воспользуемся NAPI.Task.Run
+            NAPI.Task.Run(async () =>
+            {
+                //и дальше говорим что здесь будем делать
+                var answer = (float) await playerFps.TriggerProcedure(GetFpsPlayerKey);//нам нужно получить ответ который будет состоять из того что мы затригерим процедуру(пишем ключ). И мы знаем что нам придёт float(так как до этого делили на 1) поэтому можем сразу преобразовать до него
+                //теперь игроку который вызвал эту команду нужно отправить сообщение о его фпс
+                player.SendChatMessage($"Player {playerFps.Name} fps = {answer}");
+            });
         }
     }
 }
